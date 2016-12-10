80cd8b70f14eeb48212ea98b7e7d3debe3d3ce54
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-12-10 13:09:52 -0500

    feat: added error handling for various exeptions connected to Google API

--- app.js
+++ app.js
@@ -22,7 +22,15 @@ request({
   json: true //converts string automatically to obj
 }, (error, response, body) =>{
   //console.log(JSON.stringify(body, undefined, 2));
-  console.log(`Address ${body.results[0].formatted_address}`);
-  console.log(`Lat: ${body.results[0].geometry.location.lat}`);
-  console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+  // 2 types of errors we worry from machines
+  //errors coming from google machine
+  if(error){
+    console.log('Unable to connect to google servers');
+  }else if(body.status === 'ZERO_RESULTS'){
+    console.log('Unable to find that address');
+  }else if (body.status === 'OK'){
+    console.log(`Address ${body.results[0].formatted_address}`);
+    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+  }
 });
