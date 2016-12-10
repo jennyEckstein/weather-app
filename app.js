35db9be65d6b5b54048568640c52cbd73e9a9c58
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-12-10 13:29:45 -0500

    tasj: moved featching code to a new file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
   .options({
@@ -14,23 +15,4 @@ const argv = yargs
 .alias('help', 'h')
 .argv;
 
-const ADDRESS_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-address_str = encodeURIComponent(argv.address);
-
-request({
-  url: ADDRESS_URL + address_str,
-  json: true //converts string automatically to obj
-}, (error, response, body) =>{
-  //console.log(JSON.stringify(body, undefined, 2));
-  // 2 types of errors we worry from machines
-  //errors coming from google machine
-  if(error){
-    console.log('Unable to connect to google servers');
-  }else if(body.status === 'ZERO_RESULTS'){
-    console.log('Unable to find that address');
-  }else if (body.status === 'OK'){
-    console.log(`Address ${body.results[0].formatted_address}`);
-    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
-    console.log(`Lng: ${body.results[0].geometry.location.lng}`);
-  }
-});
+geocode.geocodeAddress(argv.address);
