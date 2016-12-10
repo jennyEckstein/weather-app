1a96c11254c5f64164b4947d756c0fce4e0a0ca6
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-12-10 10:13:59 -0500

    feat: featching weather dynamically

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a:{
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetach weather for',
+      string: true //always parce a as a string
+    }
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+const ADDRESS_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+address_str = encodeURIComponent(argv.address);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=60%20west%20129%20street%20new%20york%20ny',
+  url: ADDRESS_URL + address_str,
   json: true //converts string automatically to obj
 }, (error, response, body) =>{
   //console.log(JSON.stringify(body, undefined, 2));
