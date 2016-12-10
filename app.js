f3e8bd98a577aecf0f63d9dcadb376c6077b2abd
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-12-09 21:42:01 -0500

    feat: parsing JSON for lat and lng

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=60%20west%20129%20street%20new%20york%20ny',
   json: true //converts string automatically to obj
 }, (error, response, body) =>{
-  console.log(JSON.stringify(body, undefined, 2));
+  //console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address ${body.results[0].formatted_address}`);
+  console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+  console.log(`Lng: ${body.results[0].geometry.location.lng}`);
 });
